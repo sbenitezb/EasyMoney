39f2f521a83b3d17d7408ca266a4907028ec398a
Sebastián Benítez <sebastian@ds9soft.com>
2015-02-14 13:53:19 -0300

    Initial commit

---
+++ Source/EasyMoney/EasyMoneyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EasyMoneyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EASYMONEY_API AEasyMoneyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
